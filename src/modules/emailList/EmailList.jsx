fcbd4c7106523712f70e10a358a56041b831fdd6
sohanagashimani <sohanagashimanii@gmail.com>
2023-01-21 21:26:45 +0530

    Update: favicon and spinner color

--- src/modules/emailList/EmailList.jsx
+++ src/modules/emailList/EmailList.jsx
@@ -39,7 +39,7 @@ export default () => {
       <When isTrue={isFetching}>
         <div className="h-screen w-screen flex flex-row items-center justify-center">
           <div className="flex flex-col">
-            <Spinner spinning={isFetching} />
+            <Spinner loadingColor={'#E54065'} spinning={isFetching} />
           </div>
         </div>
       </When>
@@ -80,7 +80,7 @@ export default () => {
               <When isTrue={isFetchingDetails}>
                 <div className="w-4/6 flex items-center justify-center">
                   <div className="flex flex-col">
-                    <Spinner spinning={isFetchingDetails} />
+                    <Spinner loadingColor={'#E54065'} spinning={isFetchingDetails} />
                   </div>
                 </div>
               </When>
